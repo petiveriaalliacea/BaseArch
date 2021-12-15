453fd82b335df600c20bea1e3ed908a738a42e3f
qakashi <reddit404error@gmail.com>
2021-12-15 11:05:13 +0600

    quickfix

--- src/main/java/com/qakashilliacea/service/impl/RoleServiceImpl.java
+++ src/main/java/com/qakashilliacea/service/impl/RoleServiceImpl.java
@@ -6,10 +6,17 @@ import com.qakashilliacea.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
+
+    @Override
+    public List<Role> getAll() {
+        return null;
+    }
 
     @Override
     public Role getById(Long id) {
