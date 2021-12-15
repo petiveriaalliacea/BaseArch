ae173e0a0ebe64ba68b9e9c5e52f5f4103d38e1c
qakashi <reddit404error@gmail.com>
2021-12-15 20:55:46 +0600

    TASK-6: added new utilities for password hashing

--- src/main/java/com/qakashilliacea/config/security/PasswordEncoder.java
+++ src/main/java/com/qakashilliacea/config/security/PasswordEncoder.java
@@ -1,7 +1,16 @@
 package com.qakashilliacea.config.security;
 
+import com.amdelamar.jhash.Hash;
+import com.amdelamar.jhash.algorithms.Type;
+import lombok.SneakyThrows;
+
 public class PasswordEncoder {
+    private static final String pepper = "BASE";
     public static String encode(String password) {
-        return "";
+        return Hash.password(password.toCharArray()).pepper(pepper.toCharArray()).algorithm(Type.BCRYPT).create();
+    }
+    @SneakyThrows
+    public static Boolean verifyPassword(String newPassword, String oldPassword) {
+        return Hash.password(newPassword.toCharArray()).pepper(pepper.toCharArray()).algorithm(Type.BCRYPT).verify(oldPassword);
     }
 }
