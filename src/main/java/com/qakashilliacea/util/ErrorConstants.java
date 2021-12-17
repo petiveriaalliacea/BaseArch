51d4ba8e226ae697b2772ccde5cccb9343c1c027
qakashi <reddit404error@gmail.com>
2021-12-17 20:26:12 +0600

    TASK-1: error constants

---
+++ src/main/java/com/qakashilliacea/util/ErrorConstants.java
@@ -0,0 +1,9 @@
+package com.qakashilliacea.util;
+
+public class ErrorConstants {
+    public static final Integer NOT_FOUND = 404;
+    public static final Integer OK = 200;
+    public static final Integer BAD_REQUEST = 400;
+
+    private ErrorConstants () {}
+}
