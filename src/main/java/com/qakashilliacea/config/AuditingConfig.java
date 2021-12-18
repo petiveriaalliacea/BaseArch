40c20999e609c556305aee9a9f7aa1c17c97a8a0
Shyngyskhan Kalybayev <kalybayev03@inbox.ru>
2021-12-18 16:20:51 +0600

    11

--- src/main/java/com/qakashilliacea/config/AuditingConfig.java
+++ src/main/java/com/qakashilliacea/config/AuditingConfig.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import java.time.LocalDateTime;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider", dateTimeProviderRef = "auditingDateTimeProvider")
 @EnableTransactionManagement
 @RequiredArgsConstructor
 public class AuditingConfig {
